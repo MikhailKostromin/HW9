68e292241188e6de3c32d30d6727128f9f1a09f7
Mikhail <kostrominmi@mail.ru>
2022-09-19 22:33:20 +0300

    Сумма элементов от М до Н

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  */
+
+Console.WriteLine ("Введите  значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите  значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int SummaRec(int N, int M)
+{
+    if (N == M) return M;
+    return N + SummaRec(N-1, M);
+}
+Console.WriteLine(SummaRec(M,N));
